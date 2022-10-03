9773eddf374fb3e164c699c8d1efad88093cbf43
Giancarlo Culcay <gieg5c@gmail.com>
2022-10-03 13:44:18 -0500

    React hook forms - validaciones

--- src/components/FormProduct.jsx
+++ src/components/FormProduct.jsx
@@ -1,24 +1,36 @@
 import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function FormProduct() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   const formRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(formRef.current);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData(formRef.current);
 
-    const data = {
-      title: formData.get('title'),
-      price: parseInt(formData.get('price')),
-      description: formData.get('description'),
-      categoryId: parseInt(formData.get('category')),
-      images: [formData.get('images').name],
-    };
+  //   const data = {
+  //     title: formData.get('title'),
+  //     price: parseInt(formData.get('price')),
+  //     description: formData.get('description'),
+  //     categoryId: parseInt(formData.get('category')),
+  //     images: [formData.get('images').name],
+  //   };
 
-    console.log(data);
+  //   console.log(data);
+  // };
+
+  const customSubmit = (data) => {
+    console.log('customSubmit:', data);
   };
+
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
+    <form ref={formRef} onSubmit={handleSubmit(customSubmit)}>
       <div className="overflow-hidden">
         <div className="px-4 py-5 bg-white sm:p-6">
           <div className="grid grid-cols-6 gap-6">
@@ -26,13 +38,30 @@ export default function FormProduct() {
               <label htmlFor="title" className="block text-sm font-medium text-gray-700">
                 Title
               </label>
-              <input type="text" name="title" id="title" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input
+                type="text"
+                {...register('title', { required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i })}
+                name="title"
+                id="title"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+              {errors.title?.type === 'required' && <p className="text-red-500 text-xs italic">Title is required</p>}
+              {errors.title?.type === 'maxLength' && <p className="text-red-500 text-xs italic">Title too long</p>}
+              {errors.title?.type === 'pattern' && <p className="text-red-500 text-xs italic">The title must have characters a-z</p>}
             </div>
             <div className="col-span-6 sm:col-span-3">
               <label htmlFor="price" className="block text-sm font-medium text-gray-700">
                 Price
               </label>
-              <input type="number" name="price" id="price" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input
+                type="number"
+                {...register('price', { required: true, min: 10, max: 1000 })}
+                name="price"
+                id="price"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+              {errors.price?.type === 'min' && <p className="text-red-500 text-xs italic">Price min: 10</p>}
+              {errors.price?.type === 'max' && <p className="text-red-500 text-xs italic">Price max: 1000</p>}
             </div>
             <div className="col-span-6">
               <label htmlFor="category" className="block text-sm font-medium text-gray-700">
@@ -40,6 +69,7 @@ export default function FormProduct() {
               </label>
               <select
                 id="category"
+                {...register('category', { required: true })}
                 name="category"
                 autoComplete="category-name"
                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -58,10 +88,11 @@ export default function FormProduct() {
               </label>
               <textarea
                 name="description"
+                {...register('description')}
                 id="description"
                 autoComplete="description"
                 rows="3"
-                className="form-textarea mt-1 block w-full mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className="form-textarea mt-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
               />
             </div>
             <div className="col-span-6">
@@ -83,7 +114,7 @@ export default function FormProduct() {
                         className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                       >
                         <span>Upload a file</span>
-                        <input id="images" name="images" type="file" className="sr-only" />
+                        <input id="images" {...register('images')} name="images" type="file" className="sr-only" />
                       </label>
                       <p className="pl-1">or drag and drop</p>
                     </div>
